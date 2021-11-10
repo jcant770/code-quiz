ee44a8e465c45b5d64e20743d034192e7b6a211b
Jorge <82856506+jcant770@users.noreply.github.com>
2021-11-10 04:54:01 -0500

    logic for the high scores

--- HighScores.js
+++ HighScores.js
@@ -1,26 +1,8 @@
-var highScore = document.querySelector("#highScore");
-var clear = document.querySelector("#clear");
-var goBack = document.querySelector("#goBack");
+const SC = document.getElementById('scorecont');
+var scoredata = localStorage.getItem('score');
 
-clear.addEventListener("click", function () {
-    localStorage.clear();
-    location.reload();
-});
-
-var allScores = localStorage.getItem("allScores");
-allScores = JSON.parse(allScores);
-
-if (allScores !== null) {
-
-    for (var i = 0; i < allScores.length; i++) {
-
-        var createLi = document.createElement("li");
-        createLi.textContent = allScores[i].initials + " " + allScores[i].score;
-        highScore.appendChild(createLi);
-
-    }
+function append() {
+    SC.innerHTML = scoredata + "out of ten";
 }
 
-goBack.addEventListener("click", function () {
-    window.location.replace("./index.html");
-});
+append();
